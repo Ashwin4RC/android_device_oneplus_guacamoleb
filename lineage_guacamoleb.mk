156871fb97491a24906c98d3215f4114a6b98a39
Ashwin R C <ashwin2001achu@gmail.com>
2019-06-22 04:48:28 +0000

    guacamole->guacamoleb

--- lineage_guacamoleb.mk
+++ lineage_guacamoleb.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-$(call inherit-product, device/oneplus/guacamole/device.mk)
+$(call inherit-product, device/oneplus/guacamoleb/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
