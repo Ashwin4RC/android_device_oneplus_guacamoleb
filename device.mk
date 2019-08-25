b51e343c146baa91e70da51c27411a3864d94d4b
Ashwin R C <ashwin2001achu@gmail.com>
2019-08-25 03:28:15 +0000

    guacamoleb: init from guacamole
    Change-Id: Ib745b526730a2807aa75818b5fdebac5112eb043

--- device.mk
+++ device.mk
@@ -13,13 +13,6 @@ $(call inherit-product, device/oneplus/sm8150-common/common.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
-# Camera
-PRODUCT_PACKAGES += \
-    vendor.lineage.camera.motor@1.0 \
-    vendor.lineage.camera.motor@1.0-service.guacamole \
-    OnePlusCameraHelper
-
 # Device init scripts
 PRODUCT_PACKAGES += \
-    fstab.qcom \
-    init.display.guacamole.rc
+    fstab.qcom
