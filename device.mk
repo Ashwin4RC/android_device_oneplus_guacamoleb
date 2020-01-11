ffacbf3a4a51e8fe6e741a098fbd9419d69fbdc7
Anirudh Gupta <anirudhgupta109@gmail.com>
2020-01-11 18:58:12 +0530

    hotdogb: Build tri-state-key calibration daemon
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- device.mk
+++ device.mk
@@ -48,6 +48,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
+# tri-state key
+PRODUCT_PACKAGES += \
+    tri-state-key-calibrate_daemon
+
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_SHIPPING_API_LEVEL := 29
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
